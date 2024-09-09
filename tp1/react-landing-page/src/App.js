bb40018f0807c264e2bd707355ec5fc127829215
rihem khezami <rihemkhezami5@gmail.com>
2024-09-09 15:39:08 +0100

    add tp1

---
+++ tp1/react-landing-page/src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<Header />
+<Hero />
+<Features />
+<Footer />
+</div>
+   
+  );
+}
+
+export default App;
